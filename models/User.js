d12d67b2bc7d4e15261ea8f1fe59971dca8efd98
Patty <patriciamarie0816@gmail.com>
2025-09-20 21:01:55 +0800

    connected to mongodb atlas

--- models/User.js
+++ models/User.js
@@ -1,12 +1,72 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true, 
+    validate: [validator.isEmail, "Invalid Email address"]
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    required: true,
+    // minlength: 6
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "teacher"]
+  }
+})
+
+
+userSchema.statics.register = async function (email, password, role) {
+
+  if (!email || !password || !role) {
+    throw Error("All fields must be complete")
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid")
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strongn enought")
+  }
+
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error ("Email already used")
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash, role })
+  return user
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be complete")
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Email is incorrect");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Password incorrect")
+  }
+
+  return user
+}
+
 
 module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-
+require('dotenv').config();
 
 const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
+// const cors = require("cors");
+const mongoose = require("mongoose")
 
 const userRoutes = require("./routes/userRoutes");
 
-dotenv.config();
-connectDB();
+
 
 const app = express();
 
 //Middlware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
 
 //ROutes
 
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+//mongoose connect
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("Connected to Mongodb Atlas and listening to port", process.env.PORT)
+        })
+    })
+    .catch((err) => console.error("Connection failed", err))
+
