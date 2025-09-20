7df584125732272f24276dfe57086a0dbfbf17b1
Patty <patriciamarie0816@gmail.com>
2025-09-20 11:27:12 +0800

    added user route and controllr

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
@@ -13,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 //ROutes
-const userRoutes = require("./routes/userRoutes");
+
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
