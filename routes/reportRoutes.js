bfee08e3f17570f27c95126fef7631414dbd2c5e
Patty <patriciamarie0816@gmail.com>
2025-10-23 19:22:40 +0800

    attempted exceljs file generator

---
+++ routes/reportRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const ExcelJS = require("exceljs");
+const path = require("path");
+const fs = require("fs");
+
+const router = express.Router();
+
+router.post('/generate-leave-file', async(req, res) => {
+    
+})
